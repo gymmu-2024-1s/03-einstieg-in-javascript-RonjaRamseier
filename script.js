29fd8c992508efb8f9ad15af1b4d146d12f7f903
RonjaRamseier <e263861@edu.sbl.ch>
2024-10-18 12:28:31 +0000

    Aufgabe03

--- script.js
+++ script.js
@@ -57,3 +57,45 @@ export function aufgabe02(args) {
   return result.join("")
 }
 linkupExerciseHandler("[data-click=aufgabe02]", aufgabe02)
+
+export function aufgabe03(args) {
+  const input = args
+  const result = []
+
+  let count = 0
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+
+    if (currentElement === "e") {
+      count = count + 1
+    } else if (currentElement === "E") {
+      count++
+    } else {
+      result.push(currentElement)
+    }
+  }
+  return count
+}
+
+linkupExerciseHandler("[data-click=aufgabe04]", aufgabe04)
+
+export function aufgabe03(args) {
+  const input = args
+  const result = []
+
+  let count = 0
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+
+    if (currentElement === " ") {
+      count = count + 1
+    } else if (currentElement === " ") {
+      count++
+    } else {
+      result.push(currentElement)
+    }
+  }
+  return count
+}
+
+linkupExerciseHandler("[data-click=aufgabe04]", aufgabe04)
